3407d794346254a7cfbcbb68630fb018d7a1cd22
zahidmiran <zahidmiron599@gmail.com>
2024-01-03 12:27:52 +0600

    Try to connect MongoDB

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -12,8 +12,9 @@ import { CreateUserParams, UpdateUserParams } from "@/types";
 
 export async function createUser(user: CreateUserParams) {
   try {
+    console.log("connecting DB");
     await connectToDatabase();
-
+    console.log("connected To DB");
     const newUser = await User.create(user);
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
