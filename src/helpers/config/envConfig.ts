1524dbbd4c3d6ef081ab7c60d2ef7f32321999af
zahidmiran <zahidmiron599@gmail.com>
2024-01-10 23:48:16 +0600

    changes appliedf

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,5 +1,8 @@
 export const getBaseUrl = (): string => {
-  return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api/v1";
+  return (
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    "https://review-web-backend.vercel.app/"
+  );
 };
 // export const getBaseUrl = (): string => {
 //   return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api/v1";
